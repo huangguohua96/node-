95bae95b2cfb612caab8c17fb5faa853c8f4f104
huangguohua <854836020@qq.com>
2018-09-14 22:46:35 +0800

    异步处理程序

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,25 @@
-function start() {
+const exec = require("child_process").exec; //  异步
+
+function start(response) {
     console.log("Request handler 'start' was called!");
+
+    exec("find /", 
+        { timeout: 10000, maxBuffer: 20000*1024 },  
+        function(error, stdout, stderr) {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            let time = new Date();
+            while(new Date() - time>10000){
+                response.write("hello start");
+                console.log(1111);
+            }
+            response.end();
+    });
 }
-function upload(){
+function upload(response){
     console.log("Request handler 'upload' was called!");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload!");
+    response.end();
 }
 
 exports.start = start;
